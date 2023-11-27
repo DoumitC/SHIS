1981bf48fc47d1ca55cccc2c8bc77ceb0535addb
DoumitC <119766276+DoumitC@users.noreply.github.com>
2023-11-27 09:52:47 -0600

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-#sleep 60  # Waits for 30 seconds
-
 # Run the Python script
 python3 /home/chady/shis/recognize.py &
 # Run the C++ executable
 /home/chady/shis/Main &
-
-#python3 /home/chady/mediapipe/examples/gesture_recognizer/raspberry_pi/recognize.py
